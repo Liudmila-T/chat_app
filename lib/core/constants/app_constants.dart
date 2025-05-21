58f3bfe19d8cf4fc4d7b4ed1243c962b50222c5b
liudmylatymoshenko <timlv7777@gmail.com>
2025-05-21 21:25:41 +0300

    Added a baseUrl depend on platforms

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 class AppConstants {
-  static const String baseUrl = 'http://localhost:3000';
+  static String get baseUrl => Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
+
   static const String wsUrl = 'wss://echo.websocket.events';
 
   static const Duration connectionTimeout = Duration(seconds: 10);
